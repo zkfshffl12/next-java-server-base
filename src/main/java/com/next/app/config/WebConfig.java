fb984d12f442a851c2d7d59935149d1c43237b92
박주형 <joohyung1002@naver.com>
2025-07-29 21:41:30 +0900

    (fix) swaggger update

--- src/main/java/com/next/app/config/WebConfig.java
+++ src/main/java/com/next/app/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // 모든 경로에 대해
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:8080")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
                         .allowedHeaders("*") // 모든 헤더 허용
                         .allowCredentials(true) // 인증 정보 포함 허용 (프론트에서 credentials: true 쓸 경우 필요)
